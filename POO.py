63496127e1af92f8b206ecec005298889525c5fb
BRCMOURA <brcmoura@gmail.com>
2024-09-25 21:30:57 -0300

    Add files via upload

---
+++ POO.py
@@ -0,0 +1,9 @@
+print ('O que e python?')
+print ('E um tipo de linguagem de programacao')
+
+print ('Qual programa (IDE) executa o arquivo.py')
+print ('Visual Code')
+
+print ('Qual linguagem de programacao python substitui?')
+print ('Linguagem ABC')
+ 
